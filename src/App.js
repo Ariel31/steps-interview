f281f47948c94c4bcf7485718986e314e6e5980e
Ariel31 <finarskyariel@gmail.com>
2021-11-19 12:49:09 +0200

    created tmplate virtualized list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import VirtualizedList from "./components/VirtualizedList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <VirtualizedList></VirtualizedList>
+    </div>
+  );
 }
 
 export default App;

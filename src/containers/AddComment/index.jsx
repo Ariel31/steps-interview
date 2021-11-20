6bab626ea430796dbc5b636fd079c37584ae1823
Ariel31 <53313976+Ariel31@users.noreply.github.com>
2021-11-20 18:57:01 +0200

    added post request to the steps end point

--- src/containers/AddComment/index.jsx
+++ src/containers/AddComment/index.jsx
@@ -1,18 +1,23 @@
 import { Button } from "@mui/material";
 import { useState } from "react";
 import AddCommentDialog from "../../components/Forms/AddCommentForm";
+import { addComment } from "../../services/api/comments-api";
 
 const AddComment = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
-  const addComment = async () => {};
+  const onAddComment = async comment => {
+    const addCommentResult = await addComment(comment);
+    // do something with the result
+    setOpen(false);
+  };
 
   return (
     <>
       <Button onClick={() => setOpen(!open)}>Add Comment</Button>
       <AddCommentDialog
         open={open}
-        onClick={addComment}
+        onClick={onAddComment}
         setClose={() => setOpen(false)}
       />
     </>
--- src/containers/CommentsList/index.jsx
+++ src/containers/CommentsList/index.jsx
@@ -24,24 +24,13 @@ const CommentList = () => {
       <InfiniteScroll
         style={{ height: "100%", width: "100%" }}
         className="infinite-scroll-container"
-        dataLength={comments.length} //This is important field to render the next data
+        dataLength={comments.length}
         next={getComment}
         hasMore={true}
         loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
         refreshFunction={getComment}
         pullDownToRefresh
         pullDownToRefreshThreshold={50}
-        pullDownToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-        }
-        releaseToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-        }
       >
         {comments.map((comment, index) => (
           <Item content={comment} key={index} />
